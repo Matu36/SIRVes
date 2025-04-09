24a02c1e63681b8b333dae67483ffe7d8e64407a
Matu36 <matipineda85@live.com.ar>
2025-04-09 18:26:04 -0300

    limpieza de filtros en ver agentes

--- src/components/VerAgentes.jsx
+++ src/components/VerAgentes.jsx
@@ -30,6 +30,8 @@ export default function VerAgentes() {
   const handleClean = () => {
     setFilteredData([]);
     setIsSearching("");
+    setSearchDoc("");
+    setSearchId("");
   };
 
   return (
